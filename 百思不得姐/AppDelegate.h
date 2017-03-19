c1a77738e499cb95da1a23581d63e382847ecd50
妈妈9、14 <fengzuyan@fengdembp.lan>
2017-03-19 12:16:51 +0800

    init

---
+++ 百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by Feng Zuyan on 17/3/19.
+//  Copyright © 2017年 Feng Zuyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
