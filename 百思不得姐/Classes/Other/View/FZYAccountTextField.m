641c29161e70a3e46873f2ee0c13f002e142f890
fengyanjava <fengyanjava@163.com>
2017-04-30 01:23:51 +0800

    使用KVC个性文本框placeholder颜色

--- 百思不得姐/Classes/Other/View/FZYAccountTextField.m
+++ 百思不得姐/Classes/Other/View/FZYAccountTextField.m
@@ -7,15 +7,32 @@
 //
 
 #import "FZYAccountTextField.h"
+#import <objc/runtime.h>
+
 
 @implementation FZYAccountTextField
 
 - (void)awakeFromNib {
     self.tintColor = [UIColor whiteColor];
     
-    NSDictionary *attributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
-    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:attributes];
+//    NSDictionary *attributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
+//    self.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.placeholder attributes:attributes];
     
+//    unsigned int outCount;
+//    Ivar *ivarList = class_copyIvarList([UITextField class], &outCount);
+//
+//    for (int i = 0; i < outCount; i++) {
+//        Ivar ivar = ivarList[i];
+//        NSLog(@"%s", ivar_getName(ivar));
+//    }
+//    
+//    free(ivarList);
+    
+    [self setPlaceholderTextColor:[UIColor grayColor]];
+}
+
+- (void)setPlaceholderTextColor:(UIColor *)color {
+    [self setValue:color forKeyPath:@"placeholderLabel.textColor"];
 }
 
 @end
